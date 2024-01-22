b4df4a299bb26520bb61a790211a846f430afa0f
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-01-22 20:44:38 +0530

    adding All args constructor annotation

--- src/main/java/com/springframework/springexporttoexcel/controller/CustomerController.java
+++ src/main/java/com/springframework/springexporttoexcel/controller/CustomerController.java
@@ -2,14 +2,17 @@ package com.springframework.springexporttoexcel.controller;
 
 import com.springframework.springexporttoexcel.service.CustomerService;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 import java.io.IOException;
 
-@RestController
+@Controller
 @RequestMapping("/customers")
+@AllArgsConstructor
 public class CustomerController {
     private CustomerService customerService;
     @GetMapping("/export-to-excel")
