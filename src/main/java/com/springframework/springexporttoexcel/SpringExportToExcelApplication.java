dc7f771289c532c3c796588ec89c510cb930810e
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-01-22 15:01:54 +0530

    Initial commit

---
+++ src/main/java/com/springframework/springexporttoexcel/SpringExportToExcelApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.springexporttoexcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExportToExcelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExportToExcelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springframework/springexporttoexcel/SpringExportToExcelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.springexporttoexcel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExportToExcelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
